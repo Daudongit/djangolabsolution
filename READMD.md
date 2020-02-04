5dac5fc2aadd8b817ae2ce665f5b020de1d17867
Daud <daudonmail@gmail.com>
2020-02-04 16:55:31 +0100

    Lab_5 commit

---
+++ READMD.md
@@ -0,0 +1,21 @@
+# Django Lab Solution
+
+### LAB_3
+    * Install requirement.txt
+
+### LAB_5
+    * Re-install requirement.txt
+    * create mysql database(need mysql setup)
+    * configure settings.py file and redo migration
+    ```
+    DATABASES = {
+        'default': {
+            'ENGINE': 'mysql.connector.django',
+            'NAME': 'djangolab',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+    ```
--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'djangolab',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
