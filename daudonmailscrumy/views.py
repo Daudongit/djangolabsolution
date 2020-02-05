4eaa80d9289b55fb006babb2a249f6f57a9d0f40
Daud <daudonmail@gmail.com>
2020-02-05 16:58:28 +0100

    Lab_10 commit

--- daudonmailscrumy/views.py
+++ daudonmailscrumy/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from daudonmailscrumy.models import ScrumyGoals
 
 def index(request):
-    return HttpResponse("This is a Scrum Application")
+    goals = ScrumyGoals.objects.filter(goal_name="Learn Django")
+    return HttpResponse(goals)
